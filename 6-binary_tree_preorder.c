375fbfa7d2f8d308184f07fa5219d13cde95513c
ermyas <ermyas62@gmail.com>
2024-02-28 00:16:48 -0700

    Pre-order traversa

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,3 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order
+ * @tree: pointer to the node
+ * @func: a pointer to a function
+ *
+ * Return: nothing
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
