f0ec1a9b20d447556bfea3c90b959d0c7cc06a02
Ermyas62 <ermyas62@gmail.com>
2024-02-29 05:34:12 -0800

    IS ROOT

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,9 +11,9 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node != NULL)
-		{
-			if (node->parent == NULL)
-				return (1);
-		}
+	{
+		if (node->parent == NULL)
+		return (1);
+	}
 	return (0);
 }
