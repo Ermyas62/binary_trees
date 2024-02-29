2eedc6744ce7094a3aefa617f7f93ad8cb8911b9
Ermyas62 <ermyas62@gmail.com>
2024-02-29 05:31:21 -0800

    IS LEAF

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_leaf - delete an entire binary tree
+ * binary_tree_is_leaf - delete an entire binary tree
  * @node: pointer to node to check
  *
  * Return: 1 if node is a leaf, otherwise 0
@@ -11,9 +11,9 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node != NULL)
-		{
-			if (node->left == NULL && node->right == NULL)
-				return (1);
-		}
+	{
+		if (node->left == NULL && node->right == NULL)
+			return (1);
+	}
 	return (0);
 }
